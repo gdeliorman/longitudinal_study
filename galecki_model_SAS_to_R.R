548b96e47fac42cf478e5148b1408c6a28dcdca4
gdeliorman <111044402+gdeliorman@users.noreply.github.com>
2025-04-27 23:20:57 +0200

    Add files via upload

--- galecki_model_SAS_to_R.R
+++ galecki_model_SAS_to_R.R
@@ -3,7 +3,7 @@
 
 ##for fin 1 bprs (setting 1) seperate
 var_placebo<- matrix(c(83.1577, 121.63, 121.63, 208.64), nrow=2, ncol=2)
-var_exp<- matrix(c(133.66, 186.73,186.73, 397.02 ), nrow=2, ncol=2)
+var_exp<- matrix(c(133.66, 186.73,186.73, 307.02 ), nrow=2, ncol=2)
 
 ##for fin 1 bprs (setting 1) joint
 var_joint<- matrix(c(92.0857, NA,134.17, NA,
@@ -27,8 +27,8 @@ var_placebo<- matrix(c(158.58,248.70, 248.70, 446.60), nrow=2, ncol=2)
 var_exp<- matrix(c(144.79, 230.24,230.24, 416.94 ), nrow=2, ncol=2)
 
 ##for 3-trials bprs(setting 3) joint
-var_joint<- matrix(c(158.58,248.70, 248.70, 446.60), nrow=2, ncol=2)
-var_exp<- matrix(c(144.79, 230.24,230.24, 416.94 ), nrow=2, ncol=2)
+# var_joint<- matrix(c(158.58,248.70, 248.70, 446.60), nrow=2, ncol=2)
+# var_exp<- matrix(c(144.79, 230.24,230.24, 416.94 ), nrow=2, ncol=2)
 
 ##for 3-trials cgi (setting 4)
 var_placebo<- matrix(c(158.58,248.70, 248.70, 446.60), nrow=2, ncol=2)
@@ -47,6 +47,7 @@ corr<- matrix(c( 1, NA, (var_placebo[2,1]/sqrt(var_placebo[1,1]*var_placebo[2,2]
                  (var_placebo[2,1]/sqrt(var_placebo[1,1]*var_placebo[2,2])), NA, 1, NA,
                  NA,  var_exp[2,1]/(sqrt(var_exp[1,1])*sqrt(var_exp[2,2])), NA, 1), nrow=4, ncol=4)
 
+corr<- cov2cor(Sigma)
 library(Surrogate)
 ICA<- ICA.ContCont(T0S0=corr[1,3], T1S1=corr[2,4], T0T0=Sigma[1,1], T1T1=Sigma[2,2], S0S0=Sigma[3,3], S1S1=Sigma[4,4], T0T1=seq(-1, 1, by=.1), 
              T0S1=seq(-1, 1, by=.1), T1S0=seq(-1, 1, by=.1), S0S1=seq(-1, 1, by=.1))
@@ -56,23 +57,30 @@ ICA_joint<- ICA.ContCont(T0S0=corr[1,3], T1S1=corr[2,4], T0T0=Sigma[1,1], T1T1=S
                    T0S1=seq(-1, 1, by=.1), T1S0=seq(-1, 1, by=.1), S0S1=seq(-1, 1, by=.1))
 
 
+
+ICA<- ICA_joint
 rho<- ICA$ICA
 summary(rho)
 summary(rho^2)
-p<-5 ##for fin
-#p<-6 #(0,1,2,4, and 6, 8 ) for 3 trials
+#p<-5 ##for fin
+p<-6 #(0,1,2,4, and 6, 8 ) for 3 trials
 RH_square<- 1-(1-rho^2)^p
 summary(RH_square)
+min(RH_square)
+quantile(RH_square)
+mean(RH_square)
+
 #hist(rho , main=bquote("Histogram of" ~"rho"[u]), xlab=bquote("rho"[u]))
 #hist(rho^2, main=bquote("Histogram of" ~"rho"[u]^2), xlab=bquote("rho"[u]^2))
-hist(RH_square, main=bquote("Histogram of" ~"R"[lambda]^2), xlab=bquote("R"[lambda]^2), xlim=c(0,1), breaks = 15)
-hist(RH_square, main=bquote("Histogram of" ~"R"[lambda]^2), xlab=bquote("R"[lambda]^2), xlim=c(0,1), breaks = 15)
+hist(RH_square, main="", xlab=bquote("R"[Lambda]^2), xlim=c(0,1), breaks = 15)
+hist(RH_square, xlab=bquote("R"[Lambda]^2), xlim=c(0,1), breaks = 15)
 box()
 
 len <- length(RH_square) 
-hist(RH_square, main=bquote("Histogram of" ~"R"[lambda]^2), xlab=bquote("R"[lambda]^2),  breaks = 15,prob = TRUE)
-hist(RH_square, main=bquote("Histogram of" ~"R"[lambda]^2), xlab=bquote("R"[lambda]^2),  breaks = 15, labels = TRUE, ylim = c(0,3500))
-hist(RH_square, main=bquote("Histogram of" ~"R"[lambda][j]^2), xlab=bquote("R"[lambda]^2),  breaks = 15, labels = TRUE, ylim = c(0,1500))
+hist(RH_square, main="", xlab=bquote("R"[Lambda]^2),  breaks = 15,prob = TRUE)
+hist(RH_square, main=bquote("Histogram of" ~"R"[Lambda]^2), xlab=bquote("R"[Lambda]^2),  breaks = 15, labels = TRUE, ylim = c(0,3500))
+hist(RH_square, main="", xlab=bquote("R"[Lambda]^2),  breaks = 15, labels = TRUE, ylim = c(0,1200))
+hist(RH_square, main="", xlab=bquote("R"[Lambda]^2),  breaks = 15, labels = TRUE, ylim = c(0,1600))
 
 
 grid()
@@ -80,11 +88,21 @@ box()
 summary(RH_square)
 
 
+##FOR i=1
 
+corr[1,2]<- corr[2,1]<- corr[4,1]<- corr[1,4]<- corr[2,3]<- corr[3,2] <- corr[4,3] <- corr[3,4]<- -0.9
+Sigma[1,2]<- Sigma[2,1] <- sqrt(Sigma[1,1]*Sigma[2,2])*-0.9
+Sigma[1,4]<- Sigma[4,1] <- sqrt(Sigma[1,1]*Sigma[4,4])*-0.9
+Sigma[2,3]<- Sigma[3,2] <- sqrt(Sigma[2,2]*Sigma[3,3])*-0.9
+Sigma[4,3]<- Sigma[3,4] <- sqrt(Sigma[4,4]*Sigma[3,3])*-0.9
+
+library(matrixcalc)
+
+is.positive.definite(Sigma)
 ##double check the formula using joint model 
 rho<- 0.6921
-RH_square[1467]
-first_corr<- ICA$Pos.Def[1467,]
+RH_square[1]
+first_corr<- ICA$Pos.Def[1,]
 u_11<- as.numeric(Sigma[1,1]+ Sigma[2,2]- 2*first_corr[1]* sqrt(Sigma[1,1]*Sigma[2,2]))
 u_22<- as.numeric(Sigma[3,3]+ Sigma[4,4]-2*first_corr[6]* sqrt(Sigma[3,3]*Sigma[4,4]))
 u_12<- as.numeric(Sigma[1,3]+ Sigma[2,4]- first_corr[4]* sqrt(Sigma[2,2]*Sigma[3,3])- first_corr[3]* sqrt(Sigma[1,1]*Sigma[4,4]) )
